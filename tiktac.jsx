d4a224d126af9cebb7afd732e968b58015a07bbf
bhavi-p <96447354+bhavi-p@users.noreply.github.com>
2022-05-22 11:00:33 -0400

    Add files via upload

---
+++ tiktac.jsx
@@ -0,0 +1,148 @@
+// import {checkForWinner} from './superset.js';
+
+const Square = ({ takeTurn, id }) => {
+  const mark = ["O", "X", "+"];
+  // id is the square's number
+  // filled tells us if square has been filled
+  // tik tells us symbol in square (same as player)
+  // We call takeTurn to tell Parent we have filled the square
+  const [filled, setFilled] = React.useState(false);
+  const [tik, setTik] = React.useState(2);
+  const [disable, setDisable] = React.useState(false);
+  
+
+  return (
+    <button
+      className={tik == '1' ? 'red' : 'white'}
+      disabled={disable}
+      onClick={() => {
+        setTik(takeTurn(id));
+        setFilled(true);
+        setDisable(true);
+        console.log(`Square: ${id} filled by player : ${tik}`);
+      }}
+    >
+      <h1>{mark[tik]}</h1>
+    </button>
+  );
+};
+
+const Board = () => {
+  // 1st player is X ie 1
+  // State keeps track of next player and gameState
+  const [player, setPlayer] = React.useState(1);
+  const [gameState, setGameState] = React.useState([]);
+  let status = '';
+
+  // check for winner (see superset.js)
+  // let status = `Winner is ${checkForWinner(gameState)}`;
+  if(checkForWinner(gameState) == 'No Winner Yet') {
+    status = 'Game in Progress';
+    console.log(`${status}`);
+  } else {
+    status = `Winner is ${checkForWinner(gameState)}`;
+    console.log(`We have a winner! ${status}`);
+    alert(`We have a winner! \n ${status}`);
+  }
+
+  let playerTurn = `Next Player: ${player == '0' ? 'Player O' : 'Player X'}`;  
+
+  const takeTurn = (id) => {
+    setGameState([...gameState, { id: id, player: player }]);
+    setPlayer((player + 1) % 2); // get next player
+    return player;
+  };
+  function renderSquare(i) {
+    // use properties to pass callback function takeTurn to Child
+    return <Square takeTurn={takeTurn} id={i}></Square>;
+  }
+  return (
+    <div className="game-board">
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="grid-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="grid-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div id="info">
+        <h1 className="status-style">{playerTurn}</h1>
+        <h1 className="status-style">{status}</h1>
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <Board></Board>
+    </div>
+  );
+};
+
+// check for winner
+const win = [
+  // rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // cols
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkPlayerTurn = (gameState) => {
+  return gameState.player;
+};
+
+const checkForWinner = (gameState) => {
+  // get array of box id's
+  // can't be a winner in less than 5 turns
+  if (gameState.length < 5) return 'No Winner Yet';
+  let p0 = gameState.filter((item) => {
+    if (item.player == 0) return item;
+  });
+  p0 = p0.map((item) => item.id);
+  let px = gameState.filter((item) => {
+    if (item.player == 1) return item;
+  });
+  px = px.map((item) => item.id);
+  if (p0 != null && px != null) {
+    var win0 = win.filter((item) => {
+      return isSuperset(new Set(p0), new Set(item));
+    });
+    var winX = win.filter((item) => {
+      return isSuperset(new Set(px), new Set(item));
+    });
+  }
+  if (win0.length > 0) return 'Player O ';
+  else if (winX.length > 0) return 'Player X ';
+  return 'No Winner Yet';
+};
+// check if subset is in the set
+function isSuperset(set, subset) {
+  for (let elem of subset) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
